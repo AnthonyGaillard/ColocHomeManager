d1bf16e41feed4a455c993c6fc5392fde2cf6d88
Kred <dreamlock@hotmail.fr>
2022-10-24 17:57:30 +0200

    add login route to app.js

--- app.js
+++ app.js
@@ -13,26 +13,12 @@ app
 
 sequelize.initDb()
 
-app.get('/', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, (error, data) => {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-})
-
 require('./src/routes/findAllTasks')(app)
 require('./src/routes/findTaskByPk')(app)
 require('./src/routes/createTask')(app)
 require('./src/routes/updateTask')(app)
 require('./src/routes/deleteTask')(app)
+require('./src/routes/login')(app)
 
 app.use(({res}) => {
     const message = 'Impossible de trouver la ressource demandée! Essayez un autre URL.'
