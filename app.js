e98b4a6803876a7f00aeb06f665e965be0fead89
Kred <dreamlock@hotmail.fr>
2022-10-24 17:16:35 +0200

    auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')                            // Log les requêtes pour débugguer en dev
 const bodyParser = require('body-parser')                   // Transforme automatiquement en json ou string
 const sequelize = require('./src/db/sequelize')             // ORM
+const fs = require('fs');
 
 const app = express ()
 const port = 3000
@@ -11,6 +12,21 @@ app
     .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, (error, data) => {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+})
 
 require('./src/routes/findAllTasks')(app)
 require('./src/routes/findTaskByPk')(app)
