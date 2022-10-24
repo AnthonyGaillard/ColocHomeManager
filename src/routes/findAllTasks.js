cc042f8edd4eb74fdc364a30f4a53ac00b5d5eea
Kred <dreamlock@hotmail.fr>
2022-10-24 17:52:23 +0200

    OP and auth to findAllTask

--- src/routes/findAllTasks.js
+++ src/routes/findAllTasks.js
@@ -1,7 +1,32 @@
 const { Task } = require('../db/sequelize')
+const { Op } = require('sequelize')
+const auth = require('../auth/auth')
   
 module.exports = (app) => {
-  app.get('/api/tasks', (req, res) => {
+  app.get('/api/tasks', auth, (req, res) => {
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+    
+      if(name.length < 2) {
+        const message = 'le terme recherché doit au moins contenir 2 caractères'
+        return res.status(400).json({ message })
+      }  
+
+      return Pokemon.findAndCountAll({ 
+        where: {
+          name : {                       // 'name' est la propriété du modèle Task
+            [Op.like]: `%${name}%`       // 'name' est le critère de recherche
+          } 
+        },
+        limit: limit,
+        order: ['name'] 
+      })
+      .then(({count, rows}) => {
+        const message = `Il y a ${count} tâches qui correspondent au terme de recherche ${name}.`
+        res.json({ message, data: rows})
+      })
+    } else {
     Task.findAll()
       .then(tasks => {
         const message = 'La liste des tâches a bien été récupérée.'
@@ -11,5 +36,6 @@ module.exports = (app) => {
         const message = "La liste des tâches n'a pas pu être récupérées. Réessayez dans quelques instants."
         res.status(500).json({ message, data: error })
       })
+    }
   })
 }
